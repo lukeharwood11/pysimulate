cd8c3218e310724777201ec899c6c41303f02548
Luke Harwood <43397328+lukeharwood11@users.noreply.github.com>
2022-06-07 12:44:43 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+classifiers = []
+
+with (open("README.rst")) as f: 
+  long_description = f.read()
+ 
+
+setup(
+  name="tracksim",
+  url="",
+  author="Luke Harwood",
+  author_email="mwstudiodev@gmail.com",
+  version="1.0.0",
+  description="AI Track Simulator is an easy-to-use Simulator engine built in pygame",
+  long_description=long_description,
+  packages=["simulation"],
+  include_package_data=True,
+  license="MIT License",
+  classifiers=classifiers,
+  command_options={},
+  python_requires=">=3.6",
+  extras_require={
+    "dev": ["pygame", "numpy"] 
+  }
+)
