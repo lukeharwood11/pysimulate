f64bb30d11e05253ab9cf60ee00ab3d9f0c57c10
Luke Harwood <lukeharwood11@gmail.com>
2022-09-09 17:27:50 -0500

    Issue 18 - Intermittent commit

---
+++ org.mwdev.simulator/genetic.py
@@ -0,0 +1,57 @@
+from abc import ABC
+
+from agent import Agent
+
+
+class GeneticNeuralNetwork:
+
+    def __init__(self):
+        pass
+
+    def cross_over(self, other):
+        """
+
+        :param other:
+        :return:
+        """
+        pass
+
+    def mutate(self):
+        """
+
+        :return:
+        """
+        pass
+
+
+class GeneticAlgorithmDriver(Agent, ABC):
+
+    def __init__(self, num_inputs, num_outputs):
+        super().__init__(num_inputs, num_outputs)
+
+    def update(self, inputs, reward_collision=False, wall_collision=False, keys_pressed=None) -> list[int]:
+        """
+        - Given input from the simulation make a decision
+        :param wall_collision: whether the car collided with the wall
+        :param reward_collision: whether the car collided with a reward
+        :param inputs: sensor input as a numpy array
+        :param keys_pressed: a map of pressed keys
+        :return direction: int [0 - num_outputs)
+        """
+        pass
+
+    def save_model(self, path):
+        """
+        - Save the brain of the agent to some file (or don't)
+        :param path: the path to the model
+        :return: None
+        """
+        pass
+
+    def load_model(self, path):
+        """
+        - Load the brain of the agent from some file (or don't)
+        :param path: the path to the model
+        :return: None
+        """
+        pass
